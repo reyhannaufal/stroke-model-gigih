beda87c016db62d675c379005b9dbb92448079fb
reyhannaufal <reyhannaufal21@gmail.com>
2022-06-11 11:08:01 +0700

    feat: import dataset

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,10 @@ st.title('Diagon Alley')
 
 st.write('Diagon Alley is an end-to-end ML model project that uses a combination of machine learning techniques to predict stroke ✊')
 
+#import stroke dataset
+df = pd.read_csv('./stroke_dataset.csv')
+
+# display data
+st.write(df.head())
+
+
